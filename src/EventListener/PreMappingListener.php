94478158600c6e4f321c6fe762ea4a659d38fab7
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-08-20 15:40:09 +0300

    18

--- src/EventListener/PreMappingListener.php
+++ src/EventListener/PreMappingListener.php
@@ -15,6 +15,84 @@ use Maris\Symfony\Address\Entity\Address;
 //#[AsDoctrineListener( event: Events::loadClassMetadata , priority: 100, connection: "default" )]
 class PreMappingListener
 {
+    /**
+     * Константа с полями, где ключ название в базе данных,
+     * а значение ключ в meta->fieldMappings
+     */
+    protected const MAPPING_FIELDS = [
+        "value" => "value",
+        "unrestricted_value" => "unrestricted",
+
+        "okato" => "okato",
+        "oktmo" => "oktmo",
+
+        "postal_code" => "postalIndex",
+        "postal_box" => "postalBox",
+
+        "fias_id" => "fias",
+        "region_fias_id" => "region.fias",
+        "area_fias_id" => "area.fias",
+        "city_fias_id" => "city.fias",
+        "city_district_fias_id" => "cityDistrict.fias",
+        "settlement_fias_id" => "settlement.fias",
+        "street_fias_id" => "street.fias",
+        "stead_fias_id" => "stead.fias",
+        "house_fias_id" => "house.fias",
+        "flat_fias_id" => "flat.fias",
+
+        "address_kladr_id" => "kladr",
+        "region_kladr_id" => "region.kladr",
+        "area_kladr_id" => "area.kladr",
+        "city_kladr_id" => "city.kladr",
+        "city_district_kladr_id" => "cityDistrict.kladr",
+        "settlement_kladr_id" => "settlement.kladr",
+        "street_kladr_id" => "street.kladr",
+        "stead_kladr_id" => "stead.kladr",
+        "house_kladr_id" => "house.kladr",
+        "flat_kladr_id" => "flat.kladr",
+    ];
+
+    /***
+     * Массив в котором определены по несколько ячеек
+     * которые необходимо удалить при нахождение ключа.
+     */
+    protected const MAPPING_UNION = [
+
+        "postal"=>[
+            self::MAPPING_FIELDS["postal_code"],
+            self::MAPPING_FIELDS["postal_box"],
+        ],
+
+        "fias" =>[
+            "fias",
+            "region.fias",
+            "area.fias",
+            "city.fias",
+            "cityDistrict.fias",
+            "settlement.fias",
+            "street.fias",
+            "stead.fias",
+            "house.fias",
+            "flat.fias"
+        ],
+
+        "kladr" =>[
+            "kladr",
+            "region.kladr",
+            "area.kladr",
+            "city.kladr",
+            "cityDistrict.kladr",
+            "settlement.kladr",
+            "street.kladr",
+            "stead.kladr",
+            "house.kladr",
+            "flat.kladr"
+        ],
+    ];
+
+
+
+
     /***
      * Ключи которые нужно исключить.
      * @var array
@@ -38,42 +116,21 @@ class PreMappingListener
     public function __invoke( LoadClassMetadataEventArgs $args ):void
     {
         $meta = $args->getClassMetadata();
-        match ($meta->name){
-            Address::class => $this->updateAddress( $meta ),
-            default => null
-        };
-    }
 
-    /***
-     * Обновляет сущность адреса
-     * @param ClassMetadata $m
-     * @return void
-     */
-    protected function updateAddress( ClassMetadata $m ):void
-    {
-        if(in_array("fias",$this->mappingExclude)){
-            unset($m->fieldMappings["fias"]);
-            unset($m->fieldMappings["region.fias"]);
-            unset($m->fieldMappings["area.fias"]);
-            unset($m->fieldMappings["city.fias"]);
-            unset($m->fieldMappings["cityDistrict.value"]);
-            unset($m->fieldMappings["settlement.fias"]);
-            unset($m->fieldMappings["street.fias"]);
-            unset($m->fieldMappings["stead.fias"]);
-            unset($m->fieldMappings["house.fias"]);
-            unset($m->fieldMappings["flat.fias"]);
-        }
+        if($meta->name !== Address::class)
+            return;
 
-        if(in_array("kladr",$this->mappingExclude))
-            unset($m->fieldMappings["kladr"]);
+        # Удаляем поля по названию ключа в БД.
+        foreach (self::MAPPING_FIELDS as $field => $key )
+            if( in_array( $field, $this->mappingExclude) && isset( $meta->fieldMappings[$key] ))
+                unset($meta->fieldMappings[ $key ]);
 
-        if(in_array("okato",$this->mappingExclude))
-            unset($m->fieldMappings["okato"]);
-
-        if(in_array("oktmo",$this->mappingExclude))
-            unset($m->fieldMappings["oktmo"]);
-
-        dump( $m->fieldMappings );
+        # Удаляем группы полей.
+        foreach (self::MAPPING_UNION as $exclude => $fieldList)
+            if( in_array( $exclude, $this->mappingExclude) )
+                foreach ($fieldList as $key)
+                    if(isset($meta->fieldMappings[ $key ]))
+                        unset($meta->fieldMappings[ $key ]);
     }
 
 }
