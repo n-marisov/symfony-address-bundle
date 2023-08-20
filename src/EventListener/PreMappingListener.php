baa5e37f32603dedb4ecf6a817ed98e5d0438cc4
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-08-20 17:46:06 +0300

    22

--- src/EventListener/PreMappingListener.php
+++ src/EventListener/PreMappingListener.php
@@ -2,8 +2,6 @@
 
 namespace Maris\Symfony\Address\EventListener;
 
-use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
-use Doctrine\ORM\Events;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Maris\Symfony\Address\Entity\Address;
@@ -12,7 +10,6 @@ use Maris\Symfony\Address\Entity\Address;
  * Изменяем сущности исходя из настроек непосредственно
  * перед Mapping.
  */
-//#[AsDoctrineListener( event: Events::loadClassMetadata , priority: 100, connection: "default" )]
 class PreMappingListener
 {
 
@@ -50,6 +47,10 @@ class PreMappingListener
         "postal_code" => "postalIndex",
         "postal_box" => "postalBox",
 
+        "geo_lat" => "location.latitude",
+        "geo_lon" => "location.longitude",
+
+
         "fias_id" => "fias",
         "region_fias_id" => "region.fias",
         "area_fias_id" => "area.fias",
@@ -61,7 +62,7 @@ class PreMappingListener
         "house_fias_id" => "house.fias",
         "flat_fias_id" => "flat.fias",
 
-        "address_kladr_id" => "kladr",
+        "kladr_id" => "kladr",
         "region_kladr_id" => "region.kladr",
         "area_kladr_id" => "area.kladr",
         "city_kladr_id" => "city.kladr",
@@ -71,6 +72,49 @@ class PreMappingListener
         "stead_kladr_id" => "stead.kladr",
         "house_kladr_id" => "house.kladr",
         "flat_kladr_id" => "flat.kladr",
+
+        "country_iso_code" => "country.iso",
+        "federal_district" => "federalDistrict.value",
+        "region_iso_code" => "region.iso",
+        "region_type" => "region.type",
+        "region_type_full" => "region.fullType",
+        "region_with_type" => "region.valueWithType",
+
+        "area_type" => "area.type",
+        "area_type_full" => "area.fullType",
+        "area_with_type" => "area.valueWithType",
+
+        "city_type" => "city.type",
+        "city_type_full" => "city.fullType",
+        "city_with_type" => "city.valueWithType",
+
+        "city_district_type" => "cityDistrict.type",
+        "city_district_type_full" => "cityDistrict.fullType",
+        "city_district_with_type" => "cityDistrict.valueWithType",
+
+        "settlement_type" => "settlement.type",
+        "settlement_type_full" => "settlement.fullType",
+        "settlement_with_type" => "settlement.valueWithType",
+
+        "street_type" => "street.type",
+        "street_type_full" => "street.fullType",
+        "street_with_type" => "street.valueWithType",
+
+        "stead_type" => "stead.type",
+        "stead_type_full" => "stead.fullType",
+        "stead_with_type" => "stead.valueWithType",
+
+        "house_type" => "house.type",
+        "house_type_full" => "house.fullType",
+        "house_with_type" => "house.valueWithType",
+
+        "block_type" => "block.type",
+        "block_type_full" => "block.fullType",
+        "block_with_type" => "block.valueWithType",
+
+        "flat_type" => "flat.type",
+        "flat_type_full" => "flat.fullType",
+        "flat_with_type" => "flat.valueWithType",
     ];
 
     /***
@@ -85,66 +129,150 @@ class PreMappingListener
         ],
 
         "fias" =>[
-            "fias",
-            "region.fias",
-            "area.fias",
-            "city.fias",
-            "cityDistrict.fias",
-            "settlement.fias",
-            "street.fias",
-            "stead.fias",
-            "house.fias",
-            "flat.fias"
+            self::MAPPING_FIELDS["fias_id"],
+            self::MAPPING_FIELDS["region_fias_id"],
+            self::MAPPING_FIELDS["area_fias_id"],
+            self::MAPPING_FIELDS["city_fias_id"],
+            self::MAPPING_FIELDS["city_district_fias_id"],
+            self::MAPPING_FIELDS["settlement_fias_id"],
+            self::MAPPING_FIELDS["street_fias_id"],
+            self::MAPPING_FIELDS["stead_fias_id"],
+            self::MAPPING_FIELDS["house_fias_id"],
+            self::MAPPING_FIELDS["flat_fias_id"],
         ],
 
         "kladr" =>[
-            "kladr",
-            "region.kladr",
-            "area.kladr",
-            "city.kladr",
-            "cityDistrict.kladr",
-            "settlement.kladr",
-            "street.kladr",
-            "stead.kladr",
-            "house.kladr",
-            "flat.kladr"
+            self::MAPPING_FIELDS["kladr_id"],
+            self::MAPPING_FIELDS["region_kladr_id"],
+            self::MAPPING_FIELDS["area_kladr_id"],
+            self::MAPPING_FIELDS["city_kladr_id"],
+            self::MAPPING_FIELDS["city_district_kladr_id"],
+            self::MAPPING_FIELDS["settlement_kladr_id"],
+            self::MAPPING_FIELDS["street_kladr_id"],
+            self::MAPPING_FIELDS["stead_kladr_id"],
+            self::MAPPING_FIELDS["house_kladr_id"],
+            self::MAPPING_FIELDS["flat_kladr_id"],
         ],
 
         "type"=>[
-            "region.type",
-            "area.type",
-            "city.type",
-            "cityDistrict.type",
-            "settlement.type",
-            "street.type",
-            "stead.type",
-            "house.type",
-            "flat.type"
+            self::MAPPING_FIELDS["region_type"],
+            self::MAPPING_FIELDS["area_type"],
+            self::MAPPING_FIELDS["city_type"],
+            self::MAPPING_FIELDS["city_district_type"],
+            self::MAPPING_FIELDS["settlement_type"],
+            self::MAPPING_FIELDS["street_type"],
+            self::MAPPING_FIELDS["stead_type"],
+            self::MAPPING_FIELDS["house_type"],
+            self::MAPPING_FIELDS["flat_type"]
         ],
 
         "type_full"=>[
-            "region.fullType",
-            "area.fullType",
-            "city.fullType",
-            "cityDistrict.fullType",
-            "settlement.fullType",
-            "street.fullType",
-            "stead.fullType",
-            "house.fullType",
-            "flat.fullType"
+            self::MAPPING_FIELDS["region_type_full"],
+            self::MAPPING_FIELDS["area_type_full"],
+            self::MAPPING_FIELDS["city_type_full"],
+            self::MAPPING_FIELDS["city_district_type_full"],
+            self::MAPPING_FIELDS["settlement_type_full"],
+            self::MAPPING_FIELDS["street_type_full"],
+            self::MAPPING_FIELDS["stead_type_full"],
+            self::MAPPING_FIELDS["house_type_full"],
+            self::MAPPING_FIELDS["flat_type_full"],
         ],
 
         'with_type' =>[
-            "region.valueWithType",
-            "area.valueWithType",
-            "city.valueWithType",
-            "cityDistrict.valueWithType",
-            "settlement.valueWithType",
-            "street.valueWithType",
-            "stead.valueWithType",
-            "house.valueWithType",
-            "flat.valueWithType"
-        ]
+            self::MAPPING_FIELDS["region_with_type"],
+            self::MAPPING_FIELDS["area_with_type"],
+            self::MAPPING_FIELDS["city_with_type"],
+            self::MAPPING_FIELDS["city_district_with_type"],
+            self::MAPPING_FIELDS["settlement_with_type"],
+            self::MAPPING_FIELDS["street_with_type"],
+            self::MAPPING_FIELDS["stead_with_type"],
+            self::MAPPING_FIELDS["house_with_type"],
+            self::MAPPING_FIELDS["flat_with_type"],
+        ],
+
+        "location" =>[ self::MAPPING_FIELDS["geo_lat"], self::MAPPING_FIELDS["geo_lon"] ],
+
+        "country" =>[ "country.value", self::MAPPING_FIELDS["country_iso_code"] ],
+
+        "region" =>[
+            "region.value",
+            self::MAPPING_FIELDS["region_type"],
+            self::MAPPING_FIELDS["region_type_full"],
+            self::MAPPING_FIELDS["region_with_type"],
+            self::MAPPING_FIELDS["region_iso_code"],
+            self::MAPPING_FIELDS["region_fias_id"],
+            self::MAPPING_FIELDS["region_kladr_id"],
+        ],
+        "area" =>[
+            "area.value",
+            self::MAPPING_FIELDS["area_type"],
+            self::MAPPING_FIELDS["area_type_full"],
+            self::MAPPING_FIELDS["area_with_type"],
+            self::MAPPING_FIELDS["area_fias_id"],
+            self::MAPPING_FIELDS["area_kladr_id"]
+        ],
+        "city" =>[
+            "city.value",
+            self::MAPPING_FIELDS["city_type"],
+            self::MAPPING_FIELDS["city_type_full"],
+            self::MAPPING_FIELDS["city_with_type"],
+            self::MAPPING_FIELDS["city_fias_id"],
+            self::MAPPING_FIELDS["city_kladr_id"]
+        ],
+        "city_district" =>[
+            "cityDistrict.value",
+            self::MAPPING_FIELDS["city_district_type"],
+            self::MAPPING_FIELDS["city_district_type_full"],
+            self::MAPPING_FIELDS["city_district_with_type"],
+            self::MAPPING_FIELDS["city_district_fias_id"],
+            self::MAPPING_FIELDS["city_district_kladr_id"]
+        ],
+        "settlement" =>[
+            "settlement.value",
+            self::MAPPING_FIELDS["settlement_type"],
+            self::MAPPING_FIELDS["settlement_type_full"],
+            self::MAPPING_FIELDS["settlement_with_type"],
+            self::MAPPING_FIELDS["settlement_fias_id"],
+            self::MAPPING_FIELDS["settlement_kladr_id"]
+        ],
+        "street" =>[
+            "street.value",
+            self::MAPPING_FIELDS["street_type"],
+            self::MAPPING_FIELDS["street_type_full"],
+            self::MAPPING_FIELDS["street_with_type"],
+            self::MAPPING_FIELDS["street_fias_id"],
+            self::MAPPING_FIELDS["street_kladr_id"]
+        ],
+        "stead" =>[
+            "stead.value",
+            self::MAPPING_FIELDS["stead_type"],
+            self::MAPPING_FIELDS["stead_type_full"],
+            self::MAPPING_FIELDS["stead_with_type"],
+            self::MAPPING_FIELDS["stead_fias_id"],
+            self::MAPPING_FIELDS["stead_kladr_id"]
+        ],
+        "house" =>[
+            "house.value",
+            self::MAPPING_FIELDS["house_type"],
+            self::MAPPING_FIELDS["house_type_full"],
+            self::MAPPING_FIELDS["house_with_type"],
+            self::MAPPING_FIELDS["house_fias_id"],
+            self::MAPPING_FIELDS["house_kladr_id"]
+        ],
+        "block" =>[
+            "block.value",
+            self::MAPPING_FIELDS["block_type"],
+            self::MAPPING_FIELDS["block_type_full"],
+            self::MAPPING_FIELDS["block_with_type"]
+        ],
+        "flat" =>[
+            "flat.value",
+            self::MAPPING_FIELDS["flat_type"],
+            self::MAPPING_FIELDS["flat_type_full"],
+            self::MAPPING_FIELDS["flat_with_type"],
+            self::MAPPING_FIELDS["flat_fias_id"],
+            self::MAPPING_FIELDS["flat_kladr_id"]
+        ],
 
     ];
 
